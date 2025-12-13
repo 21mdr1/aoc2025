1a2d25759312af110e8e6b24b0629619a6c94ebc
Maria Rodriguez Hertz <marurh20@gmail.com>
2025-12-12 20:47:10 -0500

    Add day 2 part 1 solution

--- day2/part1.py
+++ day2/part1.py
@@ -1,8 +1,26 @@
 input = open("./day2/inputday2.txt", "r").read()
-
 idRanges = input.split(',')
+
+total = 0
 
 for idRange in idRanges:
     start, end = map(int, idRange.split("-"))
 
+    if len(str(start)) % 2: # 1
+        startHalf = "1" + "0" * (len(str(start)) // 2)
+    else:
+        startHalf = str(start)[:len(str(start))//2]
+
+    while True:
+        num = int(startHalf + startHalf)
+
+        if num > end:
+            break
+        
+        if num >= start:
+            total += num
+        
+        startHalf = str(int(startHalf) + 1)
+
+print(total)
     
